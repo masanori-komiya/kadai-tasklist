44b8d1dc456e442d538708aea48a601b938bdd93
masanori.komiya <little_temple@icloud.com>
2020-10-11 08:28:02 +0000

    login_onetomany3

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user , except: [:index, :new]
+  before_action :correct_user , only: [:edit, :show, :update, :destroy]
   
   def index
     @tasks = Task.where(user_id: current_user.id)
